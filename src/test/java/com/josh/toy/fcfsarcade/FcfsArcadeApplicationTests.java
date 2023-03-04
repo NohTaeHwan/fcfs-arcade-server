5935ebf6b82db518dae09ed436d8b3211add4bfa
JoshNoh <thnoh93@naver.com>
2023-03-04 18:04:35 +0900

    Readme에 개요 추가

---
+++ src/test/java/com/josh/toy/fcfsarcade/FcfsArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.toy.fcfsarcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcfsArcadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
